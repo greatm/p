40245a37c7309f7057fadc6513f176f7b5314225
greatm <manoj.great@yahoo.com>
2013-05-21 20:22:07 +0530

    commit_date_05/21_time_0_20

--- p/Scripts/MyScript.js
+++ p/Scripts/MyScript.js
@@ -12,4 +12,24 @@ $(function () {
             sessionStorage.setItem("accordionMenuIndex", accordionMenuIndex);
         }
     });
+
+    //keep footer below accordionMenu
+    var hTop = $("header").position().top;
+    var hHeight = $("header").innerHeight();
+    var aTop = $("#accordionMenu").position().top;
+    var aHeight = $("#accordionMenu").innerHeight();
+    var bTop = $("#body").position().top;
+    var bHeight = $("#body").innerHeight();
+    var fTop = $("footer").position().top;
+    var footerTop = aTop + aHeight;
+    if (aHeight < bHeight) {
+        footerTop = bTop + bHeight;
+    }
+    if (footerTop > fTop) {
+        //$("footer").css({ top: (footerTop - fTop) + 'px' });
+        //$("footer").css({ top: (footerTop) + 'px' });
+        $("footer").css({ top: footerTop });
+    }
+    //
+
 });
