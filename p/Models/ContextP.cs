bc88060bbe9f98c7afce93e3c0e0dc36f6d9448d
greatm <manoj.great@yahoo.com>
2013-05-22 16:13:40 +0530

    commit_date_05/22_time_6_10

--- p/Models/ContextP.cs
+++ p/Models/ContextP.cs
@@ -29,7 +29,13 @@ namespace p.Models
                 p => p.WorkTime,
 
 
-                   new whatsnew { WorkTime = new DateTime(2013, 5, 22, 11,00, 0), Work = "create master – vendor" },
+                   //new whatsnew { WorkTime = new DateTime(2013, 5, 22, 15, 00, 0), Work = "footer area color – blue" },
+                   new whatsnew { WorkTime = new DateTime(2013, 5, 22, 15, 00, 0), Work = "footer area color – blue" },
+                   new whatsnew { WorkTime = new DateTime(2013, 5, 22, 14, 00, 0), Work = "body area color – yellow" },
+                   new whatsnew { WorkTime = new DateTime(2013, 5, 22, 13, 00, 0), Work = "menu area color – blue" },
+                   new whatsnew { WorkTime = new DateTime(2013, 5, 22, 12, 00, 0), Work = "logo area color – blue" },
+                   new whatsnew { WorkTime = new DateTime(2013, 5, 22, 11, 00, 0), Work = "add whats new in about" },
+                   new whatsnew { WorkTime = new DateTime(2013, 5, 22, 11, 00, 0), Work = "create master – vendor" },
                    new whatsnew { WorkTime = new DateTime(2013, 5, 22, 10, 10, 0), Work = "add ajax support" },
                    new whatsnew { WorkTime = new DateTime(2013, 5, 21, 10, 10, 0), Work = "add favicon" },
                    new whatsnew { WorkTime = new DateTime(2013, 5, 20, 10, 10, 0), Work = "add logo" }
